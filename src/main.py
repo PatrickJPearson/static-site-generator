4c39f5308681a287d93b110342668eeade4bb454
PatrickJPearson <pearson.patrick.j@gmail.com>
2026-02-23 12:04:11 -0500

    main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from functions.generate_pages_recursive import generate_pages_recursive
 def main():
     basepath = '/'
     if sys.argv:
-        basepath = sys.argv
+        basepath = sys.argv[1]
     copy_source("/home/pears/workspace/username/static-site-generator/docs", "/home/pears/workspace/username/static-site-generator/static", True)
     generate_pages_recursive("content", "template.html", "docs", basepath)
 main()
